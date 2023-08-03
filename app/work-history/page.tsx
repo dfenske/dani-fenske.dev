2091b637ce1e94c9f72236341d66af3f276e8d19
Dani Fenske <dani.fenske@gmail.com>
2023-08-02 19:58:55 -0700

    Scaffolding the app

---
+++ app/work-history/page.tsx
@@ -0,0 +1,21 @@
+export default function WorkHistory() {
+  return (
+    <div>
+      <h1>Work History</h1>
+      <ul>
+        <li>
+          <a href="/work-history/avanade">Work History: Avanade</a>
+        </li>
+        <li>
+          <a href="/work-history/payscale">Work History: PayScale</a>
+        </li>
+        <li>
+          <a href="/work-history/outreach">Work History: Outreach</a>
+        </li>
+        <li>
+          <a href="/work-history/square">Work History: Square</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
