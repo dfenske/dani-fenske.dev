084933c683ae08480578d3d5e14fdd0b33cf11f9
Dani Fenske <dani.fenske@gmail.com>
2023-08-04 13:50:18 -0700

    Fix animation and content-jumping

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,32 @@ import { useState, useEffect } from "react";
 export default function Home() {
   const handWavingEmoji = "\u{1F44B}";
   const message = `${handWavingEmoji} Hi, my name is Dani Fenske.`;
-  const [text, setText] = useState("");
+  const blurb =
+    "I am a full-stack software engineer in Seattle, WA, specializing in creating websites using React and TypeScript.";
+  const [heading, setHeading] = useState("");
+  const [paragraph, setParagraph] = useState("");
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      setText(
-        Array.from(message)
-          .slice(0, text.length + 1)
-          .join("")
-      );
-    }, 100);
-    return () => clearTimeout(timeout);
-  }, [text, message]);
+    if (message !== heading) {
+      const timeout = setTimeout(() => {
+        setHeading(
+          Array.from(message)
+            .slice(0, heading.length + 1)
+            .join("")
+        );
+      }, 100);
+      return () => clearTimeout(timeout);
+    } else if (blurb !== paragraph) {
+      const timeout = setTimeout(() => {
+        setParagraph(
+          Array.from(blurb)
+            .slice(0, paragraph.length + 1)
+            .join("")
+        );
+      }, 20);
+      return () => clearTimeout(timeout);
+    }
+  }, [heading, message, paragraph]);
 
   return (
     <>
@@ -32,14 +46,8 @@ export default function Home() {
         placeholder="blur"
         priority
       />
-      <h1 className="text-center my-2 blinking-cursor min-h-[6rem] md:min-h-[12rem]">
-        {text}
-      </h1>
-      <p className="mx-auto md:w-1/2 text-justify">
-        I am a software engineer in Seattle, WA. I spend my time creating
-        websites using React and TypeScript. I have also been known to venture
-        from time to time towards the backend, with Golang and C#.
-      </p>
+      <h1 className="text-center my-2 blinking-cursor">{heading}</h1>
+      <p className="mx-auto md:w-1/2 text-center">{paragraph}</p>
     </>
   );
 }
