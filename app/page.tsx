ce16b254dcb9744e933f88c54aad9b59c2995114
Dani Fenske <dani.fenske@gmail.com>
2023-08-04 13:41:52 -0700

    Add typing animation to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+
 import Image from "next/image";
 import profilePic from "../public/Headshot.png";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const handWavingEmoji = "\u{1F44B}";
+  const message = `${handWavingEmoji} Hi, my name is Dani Fenske.`;
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setText(
+        Array.from(message)
+          .slice(0, text.length + 1)
+          .join("")
+      );
+    }, 100);
+    return () => clearTimeout(timeout);
+  }, [text, message]);
+
   return (
     <>
       <Image
@@ -14,7 +32,9 @@ export default function Home() {
         placeholder="blur"
         priority
       />
-      <h1 className="text-center my-2">👋🏼 My name is Dani Fenske.</h1>
+      <h1 className="text-center my-2 blinking-cursor min-h-[6rem] md:min-h-[12rem]">
+        {text}
+      </h1>
       <p className="mx-auto md:w-1/2 text-justify">
         I am a software engineer in Seattle, WA. I spend my time creating
         websites using React and TypeScript. I have also been known to venture
