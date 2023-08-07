3f5a4a65752347337aa433ba906bebaa2b957f9e
Dani Fenske <dani.fenske@gmail.com>
2023-08-07 12:23:24 -0700

    Add tooltips to header

---
+++ app/components/header/HeaderItem.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Tooltip } from "react-tooltip";
+
+export default function HeaderItem(props: {
+  text: string;
+  href: string;
+  id: string;
+  icon: React.ReactNode;
+}) {
+  const { href, id, icon, text } = props;
+  return (
+    <>
+      <Tooltip id={id} className="tooltip" />
+      <a
+        data-tooltip-id={id}
+        href={href}
+        data-tooltip-content={text}
+        data-tooltip-place="top"
+        className="mx-2"
+      >
+        {icon}
+      </a>
+    </>
+  );
+}
--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -11,31 +11,45 @@ import {
   FiSun,
   FiTrendingUp,
 } from "react-icons/fi";
+import HeaderItem from "./HeaderItem";
 
 export default function Header() {
   const { toggleTheme, isDarkMode } = useTheme();
 
   return (
-    <header className="flex justify-between items-center text-3xl p-2">
+    <header className="header flex justify-between items-center text-3xl p-2">
       <div className="flex">
-        <a href="/" className="mx-2">
-          <FiHome />
-        </a>
-        <a href="/work-history" className="mx-2">
-          <FiTrendingUp />
-        </a>
-        <a href="/education" className="mx-2">
-          <FiAward />
-        </a>
-        <a href="/blog-posts" className="mx-2">
-          <FiEdit />
-        </a>
-        <a href="/about-me" className="mx-2">
-          <FiBookOpen />
-        </a>
-        <a href="/contact" className="mx-2">
-          <FiMail />
-        </a>
+        <HeaderItem text="Home" href="/" id="home" icon={<FiHome />} />
+        <HeaderItem
+          text="Work History"
+          href="/work-history"
+          id="work-history"
+          icon={<FiTrendingUp />}
+        />
+        <HeaderItem
+          text="Education"
+          href="/education"
+          id="education"
+          icon={<FiAward />}
+        />
+        <HeaderItem
+          text="Blog Posts"
+          href="/blog-posts"
+          id="blog-posts"
+          icon={<FiEdit />}
+        />
+        <HeaderItem
+          text="About Me"
+          href="/about-me"
+          id="about-me"
+          icon={<FiBookOpen />}
+        />
+        <HeaderItem
+          text="Contact"
+          href="/contact"
+          id="contact"
+          icon={<FiMail />}
+        />
       </div>
       <div className="flex items-center">
         <span className="mr-3 text-sm font-medium text-gray-900 dark:text-gray-300 ">
@@ -46,7 +60,7 @@ export default function Header() {
             type="checkbox"
             checked={isDarkMode}
             className="sr-only peer"
-            onClick={toggleTheme}
+            onChange={toggleTheme}
           />
           <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600" />
           <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
