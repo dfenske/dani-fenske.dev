2e2de7c8a8a7c915308f1e80b2d7b289c68000f7
dfenske <dani.fenske@outreach.io>
2023-08-07 16:09:58 -0700

    Dark mode logos

--- app/components/card/index.tsx
+++ app/components/card/index.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import Image, { StaticImageData } from "next/image";
 
 export default function Card(props: {
   header: string;
   subheader: string;
-  logoSrc: StaticImageData;
+  img: JSX.Element;
+  href: string;
 }) {
-  const { header, subheader, logoSrc: logo } = props;
+  const { header, href, subheader, img } = props;
   return (
-    <div className="p-3 dark:outline-white outline-black p-4 outline-1 outline m-3">
+    <a
+      className="p-3 dark:outline-white outline-black p-4 outline-1 outline"
+      href={href}
+    >
       <div className="text-xl">{header}</div>
       <div className="mt-1">{subheader}</div>
-      <Image src={logo} alt="logo" width={200} className="mt-4" />
-    </div>
+      <div className="mt-4">{img}</div>
+    </a>
   );
 }
--- app/work-history/page.tsx
+++ app/work-history/page.tsx
@@ -1,45 +1,39 @@
 import Card from "../components/card";
-import PayscaleLogo from "../../public/Payscale_Logo.png";
-import AvanadeLogo from "../../public/Avanade_Logo.png";
-import SquareLogo from "../../public/Square_Logo.png";
-import OutreachLogo from "../../public/Outreach_Logo.png";
+import PayscaleLogo from "../components/logos/payscale-logo";
+import AvanadeLogo from "../components/logos/avanade-logo";
+import SquareLogo from "../components/logos/square-logo";
+import OutreachLogo from "../components/logos/outreach-logo";
 
 export default function WorkHistory() {
   return (
     <>
       <h1>Work History</h1>
       <div className="">
-        <div className="flex justify-stretch">
-          <a href="/work-history/avanade" className="flex-1">
-            <Card
-              header="Avanade - Consultant"
-              subheader="Sep. 2014 - Dec. 2016"
-              logoSrc={AvanadeLogo}
-            />
-          </a>
-          <a href="/work-history/payscale" className="flex-1">
-            <Card
-              header="PayScale - Technical Lead, Sr. Software Engineer"
-              subheader="Apr. 2017 - Dec. 2021"
-              logoSrc={PayscaleLogo}
-            />
-          </a>
-        </div>
-        <div className="flex">
-          <a href="/work-history/square" className="flex-1">
-            <Card
-              header="Square - Senior Software Engineer"
-              subheader="Jan. 2021 - Aug. 2021"
-              logoSrc={SquareLogo}
-            />
-          </a>
-          <a href="/work-history/outreach" className="flex-1">
-            <Card
-              header="Outreach - Staff Software Engineer"
-              subheader="Aug. 2021 - Present"
-              logoSrc={OutreachLogo}
-            />
-          </a>
+        <div className="grid md:grid-cols-2 gap-4 ">
+          <Card
+            header="Avanade - Consultant"
+            subheader="Sep. 2014 - Dec. 2016"
+            img={<AvanadeLogo />}
+            href="/work-history/avanade"
+          />
+          <Card
+            header="PayScale - Technical Lead, Sr. Software Engineer"
+            subheader="Apr. 2017 - Dec. 2021"
+            img={<PayscaleLogo />}
+            href="/work-history/payscale"
+          />
+          <Card
+            header="Square - Senior Software Engineer"
+            subheader="Jan. 2021 - Aug. 2021"
+            img={<SquareLogo />}
+            href="/work-history/square"
+          />
+          <Card
+            header="Outreach - Staff Software Engineer"
+            subheader="Aug. 2021 - Present"
+            img={<OutreachLogo />}
+            href="/work-history/outreach"
+          />
         </div>
       </div>
     </>
