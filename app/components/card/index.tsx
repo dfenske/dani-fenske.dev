26b3828f0195d5af26d463565bb448a78f23b6be
dfenske <dani.fenske@outreach.io>
2023-08-10 11:56:58 -0700

    Refactor card to work for more scenarios

--- app/components/card/index.tsx
+++ app/components/card/index.tsx
@@ -2,20 +2,22 @@ import React from "react";
 
 export default function Card(props: {
   header: string;
-  subheader: string;
-  img: JSX.Element;
-  href: string;
+  className?: string;
+  subheader?: string;
+  img?: JSX.Element;
+  href?: string;
+  children?: React.ReactNode;
 }) {
-  const { header, href, subheader, img } = props;
-  return (
-    <a href={href}>
-      <div className="p-4">
-        <div className="text-xl min-h-[56px] border-b-[1px] border-oceanDark py-3">
-          {header}
-        </div>
-        <div className="mt-1 pt-4">{subheader}</div>
-        <div className="m-5 flex justify-center h-[100px]">{img}</div>
+  const { header, href, subheader, img, children, className } = props;
+  const content = (
+    <div className={`${className ? className : ""}`}>
+      <div className="text-xl min-h-[56px] border-b-[1px] border-oceanDark py-3">
+        {header}
       </div>
-    </a>
+      {subheader && <div className="mt-1 pt-4">{subheader}</div>}
+      {img && <div className="m-5 flex justify-center h-[100px]">{img}</div>}
+      {children}
+    </div>
   );
+  return href ? <a href={href}>{content}</a> : <>{content}</>;
 }
