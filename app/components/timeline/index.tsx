fc0636d0dcce9e11801075861965d609efb564c5
dfenske <dani.fenske@outreach.io>
2023-08-10 11:57:07 -0700

    Animate timeline items

--- app/components/timeline/index.tsx
+++ app/components/timeline/index.tsx
@@ -1,13 +1,43 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
 export default function Timeline(props: { children: React.ReactNode[] }) {
   const { children } = props;
 
+  // Start with all children hidden
+  const [visibleArr, setVisibleArr] = useState(
+    new Array(children.length).fill(false)
+  );
+
+  // Reveal each child after a delay
+  useEffect(() => {
+    const delay = 1000;
+    let timeout = 0;
+
+    children.forEach((_, index) => {
+      setTimeout(() => {
+        setVisibleArr((prevState) => {
+          const nextState = [...prevState];
+          nextState[index] = true;
+          return nextState;
+        });
+      }, timeout);
+
+      timeout += delay;
+    });
+  }, [children]);
+
+  // TODO: Need to fix the pop-in of the timeline circles
   return (
-    <div className="relative max-w-[1200px] mx-auto timeline">
+    <div className="timeline">
       {children.map((child, i) => {
         return (
           <div
-            key={`{i}-child`}
-            className={`container ${i % 2 === 0 ? "left" : "right"}`}
+            key={i}
+            className={`container transition-opacity duration-500 ease-in-out 
+            ${i % 2 === 0 ? "left" : "right"} ${
+              visibleArr[i] ? "opacity-100" : "opacity-0"
+            }`}
           >
             <div className="content">{child}</div>
           </div>
