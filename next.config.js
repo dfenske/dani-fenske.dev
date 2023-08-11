bc68fcefdb01be22644fbb870d557f16c26a5de0
dfenske <dani.fenske@outreach.io>
2023-08-11 11:29:47 -0700

    Attempt to fix remote image URLs by editing next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "miro.medium.com",
-        port: "",
-        pathname: "/v2/**",
-      },
-    ],
+    domains: ["miro.medium.com"],
   },
 };
 
