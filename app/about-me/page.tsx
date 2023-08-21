3492b8c5bffadf66ea205f4a12b87a902bb5b2fd
dfenske <dani.fenske@outreach.io>
2023-08-21 15:24:07 -0700

    Update about me content

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -19,31 +19,45 @@ export default function AboutMe() {
           <h2>Fun facts</h2>
           <ul className="list-disc ml-4">
             <li>
-              I like to change my hair color often, as seen by the difference
-              between my{" "}
-              <a
-                aria-label="LinkedIn page"
-                href="https://www.linkedin.com/in/daniellefenske/"
-              >
-                LinkedIn photo
-              </a>{" "}
-              and my{" "}
-              <a aria-label="Github page" href="https://github.com/dfenske">
-                Github photo
-              </a>
-              .
+              I was born and raised in Seattle. My sister and I are
+              third-generation software engineers! &#x1F604;
             </li>
-            <li>I was born and raised in Seattle.</li>
             <li>
-              I am a third-generation software engineer. My sister is a dev as
-              well! &#x1F604;
+              I listen to an inordinate amount of podcasts. The topics range
+              widely, from personal finance (
+              <a
+                href="https://www.biggerpockets.com/podcasts/money"
+                target="_blank"
+              >
+                Bigger Pockets Money
+              </a>
+              ), to gardening, (
+              <a
+                href="https://open.spotify.com/show/5nOQHxh2cGs4k8bDYnQ1fr"
+                target="_blank"
+              >
+                The Beet
+              </a>
+              ), to good, old fashioned gossip (
+              <a
+                target="_blank"
+                href="https://open.spotify.com/show/0KVZ16mLZ1bbNlnKemYTzm"
+              >
+                Normal Gossip
+              </a>
+              ).
+            </li>
+            <li>
+              I have a terrible sense of direction, and I am constantly getting
+              a little bit lost. I like to think of the times I get turned
+              around as &quot;life side-quests&quot;.
             </li>
           </ul>
           <h2>Where you might find me </h2>
           <ul className="list-disc ml-4 flex-1">
             <li>
               Poking around my veggie garden. I have about 400ft&sup2; of raised
-              beds. This year&apos;s garden-success award goes to my 1 pound
+              beds. This year&apos;s garden-success award goes to my 2 pound
               tomato.
             </li>
             <li>Plucking the banjo, my pandemic hobby of choice.</li>
